564ad88af645da1cb461800bd6ef74d446b4c63d
DenisDoroshko <de.doroshko@mail.ru>
2020-11-18 18:10:32 +0300

    first commit

---
+++ Task1/Bakery/BakeryProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bakery
+{
+    public enum ProductTypes 
+    { 
+        Bread,
+        Baton,
+        Bun,
+        Pie,
+        Cake
+    }
+
+    public abstract class BakeryProduct
+    {
+        private ProductTypes type;
+        public ProductTypes Type { get { return type; } }
+        public abstract int MarkUp { get; set; }
+    }
+}
